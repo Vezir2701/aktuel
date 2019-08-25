bfd0aabcd50ae5654666ded5a1931eecf9e71bcf
Vezir2701 <54472582+Vezir2701@users.noreply.github.com>
2019-08-25 22:50:34 +0300

    Add files via upload

---
+++ aktuel.js
@@ -0,0 +1,29 @@
+{"aktueller":
+		[
+
+			{
+				"id":1,
+				"isim":"bim",
+				"marka_logo":"",
+				"aktuel_resim":"",
+				"tarih":"10.10.2019"	
+				},
+
+{
+				"id":2,
+				"isim":"bim",
+				"marka_logo":"",
+				"aktuel_resim":"",
+				"tarih":"10.10.2019"	
+				},
+{
+				"id":3,
+				"isim":"bim",
+				"marka_logo":"",
+				"aktuel_resim":"",
+				"tarih":"10.10.2019"	
+				}			
+
+		]
+
+}
